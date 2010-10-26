42fbb350a0d37949d662475760d04ce696fb39c9
wojciech.sznapka <wojtek@sznapka.pl>
2010-10-26 16:31:03 +0200

    Removing build.xml file if exists

--- build_remote.sh
+++ build_remote.sh
@@ -4,9 +4,16 @@ host="10.254.254.94"
 user="wowo"
 keys="/var/keys/xs33_id_rsa"
 path="/home/wowo/localhost/CarCalc"
+file="build.xml"
+
+echo ">> build     Removing existing $file file"
+if [ -e "$file" ]
+then
+  rm  $file
+fi
 
 echo ">> build     Connecting via ssh and running build.sh script"
-ssh $user@$host -i $keys "cd $path && ./build.sh --xml=build.xml"
+ssh $user@$host -i $keys "cd $path && ./build.sh --xml=$file"
 
-echo ">> build     Downloading build.xml"
-scp -i $keys $user@$host:$path/build.xml .
+echo ">> build     Downloading $file"
+scp -i $keys $user@$host:$path/$file .
