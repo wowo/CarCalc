8dfd1bbe10d049547a959b4eac8884b445951360
wojciech.sznapka <wojtek@sznapka.pl>
2010-10-26 16:18:37 +0200

    Fixed build remote - proper path to the keys

--- build_remote.sh
+++ build_remote.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+host="10.254.254.94"
+user="wowo"
+keys="/var/keys/xs33_id_rsa"
+path="/home/wowo/localhost/CarCalc"
+
 echo ">> build     Connecting via ssh and running build.sh script"
-ssh wowo@10.254.254.94 -i xs33_id_rsa "cd /home/wowo/localhost/CarCalc && ./build.sh --xml=build.xml"
+ssh $user@$host -i $keys "cd $path && ./build.sh --xml=build.xml"
 
 echo ">> build     Downloading build.xml"
-scp -i xs33_id_rsa  wowo@10.254.254.94:/home/wowo/localhost/CarCalc/build.xml .
+scp -i $keys $user@$host:$path/build.xml .
